b7b39aa137fdb455c340b1b6578a557b54a393d0
subramanyaraju1112 <subramanyaraju1112@gmail.com>
2024-06-01 17:51:06 +0530

    styles added

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -36,20 +36,28 @@ const UsersList = () => {
     };
 
     return (
-        <section className='p-4 flex flex-col gap-4 items-center border border-[0.063rem] border-[#3f4856] rounded-md shadow-lg'>
-            <div>
-                <h1 className='text-3xl font-bold text-[#3f4856]'>List Of Users</h1>
+        <section className='flex flex-col gap-4 border border-[0.063rem] border-[#3f4856] rounded-md shadow-lg'>
+            <div className='p-4 w-full bg-[#0e2c50]'>
+                <h1 className='text-3xl font-semibold text-white'>List Of Users</h1>
             </div>
-            <div>
+            <div className='px-4'>
                 <ul>
                     {users.map((user) => {
                         return (
-                            <li key={user.id} className='p-2 list-disc text-left text-lg text-[#3f4856] border-b-[0.063rem] border-[#E8EBEF]'>{user.FirstNameLastName}</li>
+                            <li key={user.id} className='p-2 flex justify-between items-start border-b-[0.063rem] border-[#E8EBEF]'>
+                                <div className='flex flex-col gap-1 items-start'>
+                                    <span className='list-disc text-left text-lg text-[#3f4856]'>{user.FirstNameLastName}</span>
+                                    <span className='opacity-50 text-sm text-[#252525]'>{user.JobTitle}</span>
+                                </div>
+                                <div>
+                                    <span className='opacity-60 text-xs text-[#3f4856]'>Ph: {user.Phone}</span>
+                                </div>
+                            </li>
                         )
                     })}
                 </ul>
             </div>
-            <div className='flex gap-1 items-center'>
+            <div className='p-4 flex gap-1 items-center'>
                 <button className='p-2 flex gap-1 items-center border border-[#3F4856] hover:cursor-pointer hover:bg-[#3f4856] hover:text-white' onClick={handlePrevPage} disabled={currentPage === 0}>
                     <img src={Previous} alt='next-icon' width={7} height={7} />
                     <span>Previous</span>
